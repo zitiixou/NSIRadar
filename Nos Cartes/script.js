4342287157ccf0270d57223c2c9bb874efdd62dc
uncoeurgris <93775133+uncoeurgris@users.noreply.github.com>
2023-05-21 13:35:56 +0200

    Add files via upload

---
+++ Nos Cartes/script.js
@@ -0,0 +1,36 @@
+function changerLeTheme () {
+    //On construit une liste de couleur
+    const couleurs = ['#4e54c8','#003AFF','#FF00C5','#008B90', '#06B41B', '#C89400']
+
+      //On genere un indice de couleur au hasard
+      let randomNumber = Math.floor(Math.random() * 6)
+
+      //on recupere la couleur actuel
+      couleurActuel = localStorage.getItem('couleur')
+  
+      //si la couleur actuel est la meme que celle tirée au hasard on en retire une
+      while ( couleurs[randomNumber] === couleurActuel ) {
+          randomNumber = Math.floor(Math.random() * 6)
+      }
+
+
+    //On applique les couleur au element
+    localStorage.setItem("couleur", couleurs[randomNumber]);
+
+    window.location.reload()
+}
+
+//Au chargement de la page on recupere la couleur et on l'applique aux elements
+window.addEventListener("load", ()=> {
+    const fond = document.getElementById('Fond--Div')
+    const bouttons = document.getElementById('carte--A')
+    const menus = document.querySelectorAll('.Menu--P')
+
+    couleurActuel = localStorage.getItem('couleur')
+    fond.style.backgroundColor = couleurActuel
+    bouttons.style.backgroundColor = couleurActuel
+    menus.forEach((menu)=> {
+        menu.style.backgroundColor = couleurActuel
+    })
+});
+
